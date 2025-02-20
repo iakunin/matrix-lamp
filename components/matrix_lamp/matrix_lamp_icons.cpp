fbba4ee78d24dea29d24cece8744502ddee2dbab
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-02-20 23:40:56 +0200

    2025.2.13: Update for ESPHome 2025.2

--- components/matrix_lamp/matrix_lamp_icons.cpp
+++ components/matrix_lamp/matrix_lamp_icons.cpp
@@ -4,14 +4,13 @@ namespace esphome {
 namespace matrix_lamp {
 
 #if defined(MATRIX_LAMP_USE_DISPLAY)
-MatrixLamp_Icon::MatrixLamp_Icon(const uint8_t *data_start, int width, int height, uint32_t animation_frame_count, esphome::image::ImageType type, std::string icon_name, bool revers, uint16_t frame_duration)
-    : Animation(data_start, width, height, animation_frame_count, type)
+MatrixLamp_Icon::MatrixLamp_Icon(const uint8_t *data_start, int width, int height, uint32_t animation_frame_count, esphome::image::ImageType type, std::string icon_name, bool revers, uint16_t frame_duration, esphome::image::Transparency transparency)
+    : Animation(data_start, width, height, animation_frame_count, type, transparency)
 {
   this->name = icon_name;
   this->reverse = revers;
   this->frame_duration = frame_duration;
   this->counting_up = true;
-  this->transparent_=false;
 }
 
 void MatrixLamp_Icon::next_frame()
