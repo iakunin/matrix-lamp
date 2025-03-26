63c21346e3dfb7362ddc5094672ee839849b44ce
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-03-26 17:39:24 +0200

    Update matrix_lamp.cpp

--- components/matrix_lamp/matrix_lamp.cpp
+++ components/matrix_lamp/matrix_lamp.cpp
@@ -4,16 +4,6 @@
 #include "utility.h"
 #include "effect_ticker.h"
 
-#ifdef __riscv
-
-#undef __INT32_TYPE__
-#define __INT32_TYPE__      int
-
-#undef __UINT32_TYPE__
-#define __UINT32_TYPE__     unsigned int
-
-#endif // __riscv
-
 namespace esphome {
 namespace matrix_lamp {
 
